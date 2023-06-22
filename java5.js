b0f32a8b5e8867d79b16a7b29fcd6338792f1c89
Abdulhalim acar <109023093+abdulhalimacar@users.noreply.github.com>
2023-06-22 08:21:34 -0700

    Add files via upload

---
+++ java5.js
@@ -0,0 +1,42 @@
+
+/*
+for(let i =1;i<=5; i++){
+    console.log(i);
+}
+*/
+
+/*first question */
+
+
+// const n = Number(prompt("enter a number:"));
+// let summ=0;
+
+// for(let i = 1; i<=n; i++) {
+//     summ += i;
+//     /*süreci görmek için sum,i takip edilebilir */
+//     console.log(summ,i)
+// }
+
+// console.log(`the sum is equal to ${summ}`);
+
+// 0-100 arasında 10 adet rastegele sayı üreten kodu for ile yaz//
+
+// for(let i=1;i<=10;i++){
+//     const rastgele = Math.random()*100;
+//     console.log(rastgele);
+// }
+
+// math floor hep aşağıdaki tam sayıya yuvarlar(20.99 çıksa bile 20'ye yuvarlar)
+
+// math ceil sürekli yukarıya yuvarlar(20.01 çıksa bile 21'e yuvarlar bu)
+// math round değerine göre yuvarlar 
+// math trunc kesirli sayının tam kısmını alır
+
+
+// for(let i=1;i<=10;i++){
+//     const rastgele =Math.round(Math.random()*100);
+//     console.log(rastgele);
+// }
+
+
+
